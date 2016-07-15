dcdb93ea5976ac07f98053073b26ef90cf17e649
Schulstad Espen <Espen.Schulstad@telenor.com>
2016-07-15 10:12:37 +0200

    Initial Commit

---
+++ mapgame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapgame
+//
+//  Created by Espen Schulstad on 15.07.2016.
+//  Copyright © 2016 Espen Schulstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
